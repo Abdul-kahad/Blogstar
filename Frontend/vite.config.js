521d066c5b3572673c2cb546e90e02755b2d8a8b
Abdul Kahad <abdulkahad0500@gmail.com>
2025-12-02 18:07:46 -0800

    changed base route

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/',
-   build: {
-    outDir: 'build'
+  base: '/',
+  build: {
+    // Vercel expects the build output to be in 'dist' by default.
+    // Changing this ensures Vite puts the optimized files where Vercel looks.
+    outDir: 'dist'
   }
 })
